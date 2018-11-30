2355eaa80937f2e63363de72ce61b9e442c09c5a
nasuton <lovetennis_airu@yahoo.co.jp>
2018-12-01 00:57:38 +0900

    とりあえずタイトルシーンからゲームメインに行けるようにした。

---
+++ UnitySide/Assets/Script/Othrer/TitleScene.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleScene : MonoBehaviour
+{
+    private void Update()
+    {
+        //Enterキーが押された場合は、ランキングシーンへ
+        if(Input.GetKeyDown(KeyCode.Return))
+        {
+            SceneManager.LoadScene("RankingScene");
+        }
+
+        //Spaceキーが押された場合は、ゲームスタート
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            SceneManager.LoadScene("GameMainScene");
+        }
+    }
+}
